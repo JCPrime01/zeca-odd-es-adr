346fc9d5365d2b992745246c35b6e8db50b79dfc
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-28 12:39:57 -0300

    chore: update shareCode to PFXKGXORZLL

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/xkczsekz?shareCode=A2SEPCT3KN3&afp=sb' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/xkczsekz?shareCode=PFXKGXORZLL&afp=sb' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
