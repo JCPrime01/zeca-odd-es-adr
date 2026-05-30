f8ef05627ff94f633e1f4f98498dc1d703f1b931
JCPrime01 <vanzokiller@gmail.com>
2026-05-30 10:54:28 -0300

    chore: shareCode PFXKGXORZLL -> Q5PBWJK7BB2 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/xkczsekz?shareCode=PFXKGXORZLL&afp=sb' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/xkczsekz?shareCode=Q5PBWJK7BB2&afp=sb' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
